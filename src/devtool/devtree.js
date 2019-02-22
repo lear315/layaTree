94d34d5ed1040f862b9612cb5b5cd0925ddf534f
618021 <618021@nd.com>
2019-02-22 16:54:43 +0800

    版本更新

--- src/devtool/devtree.js
+++ src/devtool/devtree.js
@@ -5,11 +5,13 @@ import devtree from './devtree.vue';
 import node2dproperty from './node2dproperty.vue';
 import node3dproperty from './node3dproperty.vue';
 import uiprop from '../comp/uiprop.vue';
+import VueResource from 'vue-resource';
 
 Vue.component('uiprop', uiprop);
 Vue.component('node2dproperty', node2dproperty);
 Vue.component('node3dproperty', node3dproperty);
 
+Vue.use(VueResource);
 Vue.use(ElementUI);
 new Vue({
     el: '#devtree',
--- src/devtool/devtree.vue
+++ src/devtool/devtree.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="devtree">
+		<div> {{serverInfo}}</div>
 		<el-button type="success" class="el-icon-refresh" size="mini" @click="onBtnClickUpdatePage">捕获刷新</el-button>
 		<div v-show="isShowDebug">
 			<el-row>
@@ -51,9 +52,28 @@
 				},
 				treeData: [],
 				nodeInfoType: 0,
+				serverInfo: ""
 			}
 		},
+
 		created() {
+			this.$http.get(chrome.extension.getURL('manifest.json'), {
+			}).then(function(response){  
+				var version = response.body.version;
+				// http://193.112.105.97:8066/updateInfo
+				this.$http.get('http://127.0.0.1:8066/updateInfo?version=' + version, {
+				}).then(function(response){  
+					// response.data中获取ResponseData实体
+					this.serverInfo = response.bodyText;
+				},function(response){  
+					// 发生错误
+					this.serverInfo = "服务器欠费啦~~"
+				});
+			},function(response){  
+				// 发生错误
+				this.serverInfo = "codeAI检测结果:你的代码风格过时了~~"
+			});
+
 			if (chrome && chrome.extension) {
 
 			} else {
@@ -95,74 +115,74 @@
 			window.addEventListener('message', function (event) {
 					console.log("on vue:" + JSON.stringify(event.data));
 					console.log("on vue:" + JSON.stringify(event));
-				}, false);
+			}, false);
+		},
+
+		methods: {
+			handleNodeClick(data) {
+				let exId = data.exId;
+				if (exId !== undefined) {
+					let code = "window.getNodeInfo('" + exId + "')";
+					chrome.devtools.inspectedWindow.eval(code);
+				}
 			},
 
-			methods: {
-				handleNodeClick(data) {
-					let exId = data.exId;
-					if (exId !== undefined) {
-						let code = "window.getNodeInfo('" + exId + "')";
-						chrome.devtools.inspectedWindow.eval(code);
+			updateView(data) {
+				// 构建树形数据
+				this.treeData = [];
+				let stageData = data.stage;
+				if (stageData) {
+					let dataRoot = {
+						type: stageData.type, 
+						exId: stageData.exId,
+						label: stageData.name, 
+						children: []
+					};
+					this.treeData.push(dataRoot);
+					this.handleNodeClick(dataRoot);
+
+					for (let k in stageData.children) {
+						let itemData = stageData.children[k];
+						let nodeItem = {};
+						dealChildrenNode(itemData, nodeItem);
+						this.treeData[0].children.push(nodeItem);
 					}
-				},
+				}
 
-				updateView(data) {
-					// 构建树形数据
-					this.treeData = [];
-					let stageData = data.stage;
-					if (stageData) {
-						let dataRoot = {
-							type: stageData.type, 
-							exId: stageData.exId,
-							label: stageData.name, 
-							children: []
-						};
-						this.treeData.push(dataRoot);
-						this.handleNodeClick(dataRoot);
-
-						for (let k in stageData.children) {
-							let itemData = stageData.children[k];
-							let nodeItem = {};
-							dealChildrenNode(itemData, nodeItem);
-							this.treeData[0].children.push(nodeItem);
-						}
+				function dealChildrenNode(rootData, obj) {
+					obj['data'] = rootData;
+					obj['exId'] = rootData.exId;
+					obj['label'] = rootData.name;
+					obj['children'] = [];
+					let rootChildren = rootData.children;
+					for (let k in rootChildren) {
+						let itemData = rootChildren[k];
+						let item = {};
+						dealChildrenNode(itemData, item);
+						obj.children.push(item);
 					}
+				}
+			},
 
-					function dealChildrenNode(rootData, obj) {
-						obj['data'] = rootData;
-						obj['exId'] = rootData.exId;
-						obj['label'] = rootData.name;
-						obj['children'] = [];
-						let rootChildren = rootData.children;
-						for (let k in rootChildren) {
-							let itemData = rootChildren[k];
-							let item = {};
-							dealChildrenNode(itemData, item);
-							obj.children.push(item);
-						}
-					}
-				},
+			getInjectScriptString() {
+				let code = treeinject.toString();
+				let array = code.split('\n');
+				array.splice(0, 1);// 删除开头
+				array.splice(-1, 1);// 删除结尾
+				let evalCode = "";
+				for (let i = 0; i < array.length; i++) {
+					evalCode += array[i] + '\n';
+				}
+				return evalCode;
+			},
 
-				getInjectScriptString() {
-					let code = treeinject.toString();
-					let array = code.split('\n');
-					array.splice(0, 1);// 删除开头
-        			array.splice(-1, 1);// 删除结尾
-					let evalCode = "";
-					for (let i = 0; i < array.length; i++) {
-						evalCode += array[i] + '\n';
-					}
-					return evalCode;
-				},
-
-				onBtnClickUpdatePage() {
-					let code = this.getInjectScriptString();
-					chrome.devtools.inspectedWindow.eval(code, function (result, e) {
-						console.log("刷新成功!");
-					});
-				},
-    }
+			onBtnClickUpdatePage() {
+				let code = this.getInjectScriptString();
+				chrome.devtools.inspectedWindow.eval(code, function (result, e) {
+					console.log("刷新成功!");
+				});
+			},
+    	}
   }
 </script>
 
